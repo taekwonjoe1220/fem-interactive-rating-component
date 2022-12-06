97280b0579ca4745abcaf25ea659bcd44876205c
taekwonjoe1220 <75289380+taekwonjoe1220@users.noreply.github.com>
2022-12-06 16:11:26 -0700

    minor updates
    - Added formatting to submit state (almost done)
    - Added hover and active states to ratings radio labels
    - Added "hidden" class to toggle displayed html
    - Updated section ids to classes for specificity override of hidden class to work
    - Created global variables for testing active states of ratings labels

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const ratings = document.querySelectorAll('.ratings');
+const radios = document.querySelectorAll('.radio');
+
+const checkedToggle = (e) => {
+
+}
+
+// click event trigger on radio
+  // if current radio selected is already checked, do nothing
+
+  // if current radio not checked iterate over array of radios and remove all instance of classs "checked"
+  // add class "checked" to currently clicked radio button label to update css
+  // update checked radio value (array index) to use for on submit function
+
+
+// on submit function
+  // remove primary state
+  // render secondary state
+    // use stored value of array index to display what rating was selected on secondary state
+    // toggle display class of primary state to hide it
+    // toggle display class of secondary state to show it
+    // Dynamically render html? ** update to this later **
